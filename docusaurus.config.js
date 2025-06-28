07c29fc07b459f97ff345c25faf69ce1a173d0a3
Salman Faris <farissalmannbr@gmail.com>
2025-06-29 02:11:13 +0530

    update baseUrl and rl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://salmanfarisvp.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/makertrain-fab25/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
