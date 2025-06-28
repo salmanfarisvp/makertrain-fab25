41b01248ab9be605360d345e57931355ff03de9c
Salman Faris <farissalmannbr@gmail.com>
2025-06-29 01:48:00 +0530

    updated github lin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/salmanfarisvp/makertrain-fab25',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -96,7 +96,7 @@ const config = {
           },
           //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/salmanfarisvp/makertrain-fab25',
             label: 'GitHub',
             position: 'right',
           },
